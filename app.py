983024096ff38d5ebdd5fba0b77dcdd5fae5cf8b
AEGIR24 <160857424+AEGIR24@users.noreply.github.com>
2024-05-28 11:36:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,16 +29,22 @@ if uploaded_file is not None:
    # Filtrar el DataFrame utilizando query
    filtrado_df_min = df1.query(f"`temperatura ESP32` > {min_temp}")
    # Mostrar el DataFrame filtrado
+
+   
    st.subheader("Temperaturas superiores al valor configurado.")
    st.write('Dataframe Filtrado')
+   
    st.write(filtrado_df_min)
    
    max_temp = st.slider('Selecciona valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
    # Filtrar el DataFrame utilizando query
    filtrado_df_max = df1.query(f"`temperatura ESP32` < {max_temp}")
    # Mostrar el DataFrame filtrado
+
+   
    st.subheader("Temperaturas Inferiores al valor configurado.")
    st.write('Dataframe Filtrado')
+   
    st.write(filtrado_df_max)
    
 
